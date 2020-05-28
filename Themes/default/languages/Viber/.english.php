f577b97c100c4f90bd9e7fca865dfbfa9934e68b
satsis <maxya@mail.ru>
2020-05-28 16:17:03 +0300

    сомнительный перевод

--- Themes/default/languages/Viber/.english.php
+++ Themes/default/languages/Viber/.english.php
@@ -1,10 +1,15 @@
 <?php
 
-$txt['viber_settings']           = 'Viber уведомления';
-$txt['viber_desc']               = 'Здесь можно изменить настройки viber.';
-$txt['viber_enable']             = 'Включить или выключить работу Viber';
-$txt['viber_where_is']           = array('Выключено', 'Включено');
-$txt['viber_api_title']          = 'Настройка API key для viber бота';
-$txt['viber_api']                = 'Текущий ключ <div class="smalltext">Без пробелов.</div>';
-$txt['viber_api_set']            = '451e54f24e1002ee-170c2356302871eb-68a6fe79d548fb9c';
-$txt['viber_api_desc']           = 'Необходимо добавить API key viber: ' . $txt['viber_api_set'];
+$txt ['viber_settings'] = 'Viber notifications';
+$txt ['viber_desc'] = 'Here you can change the viber settings.';
+$txt ['viber_enable'] = 'Turn Viber on or off';
+$txt ['viber_enable_desc'] = 'Enabling the module will add a button to the site menu';
+$txt ['viber_url'] = 'URL of the viber bot';
+$txt ['viber_url_desc'] = 'You can specify the url of the form viber: // public? id = sat or on the page with the description of the bot';
+$txt ['viber_button_after'] = 'The viber button after';
+$txt ['viber_button_after_desc'] = 'Specify after which menu item to display the button, by default search';
+$txt ['viber_button_after_deaf'] = 'search';
+$txt ['viber_api_title'] = 'Configure API key for viber bot';
+$txt ['viber_api'] = 'Current key <div class = "smalltext"> No spaces. </div>';
+$txt ['viber_api_set'] = '451e54f24e1002ee-170c2356302871eb-68a6fe79d548fb9c';
+$txt ['viber_api_desc'] = 'You need to add the API key viber:'. $txt ['viber_api_set'];
--- Themes/default/languages/Viber/.ukrainian.php
+++ Themes/default/languages/Viber/.ukrainian.php
@@ -1,10 +1,15 @@
 <?php
 
-$txt['viber_settings']           = 'Viber уведомления';
-$txt['viber_desc']               = 'Здесь можно изменить настройки viber.';
-$txt['viber_enable']             = 'Включить или выключить работу Viber';
-$txt['viber_where_is']           = array('Выключено', 'Включено');
-$txt['viber_api_title']          = 'Настройка API key для viber бота';
-$txt['viber_api']                = 'Текущий ключ <div class="smalltext">Без пробелов.</div>';
-$txt['viber_api_set']            = '451e54f24e1002ee-170c2356302871eb-68a6fe79d548fb9c';
-$txt['viber_api_desc']           = 'Необходимо добавить API key viber: ' . $txt['viber_api_set'];
+$txt [ 'viber_settings'] = 'Viber повідомлення';
+$txt [ 'viber_desc'] = 'Тут можна змінити налаштування viber.';
+$txt [ 'viber_enable'] = 'Включити або вимкнути роботу Viber';
+$txt [ 'viber_enable_desc'] = 'Включення модуля додасть кнопку в меню сайту';
+$txt [ 'viber_url'] = 'URL viber бота';
+$txt [ 'viber_url_desc'] = 'Ви можете вказати url виду viber: // public? Id = sat або на страніцуу з описом бота';
+$txt [ 'viber_button_after'] = 'Кнопка viber після';
+$txt [ 'viber_button_after_desc'] = 'Вкажіть після якого пункту меню відображати кнопку, за замовчуванням search';
+$txt [ 'viber_button_after_deaf'] = 'search';
+$txt [ 'viber_api_title'] = 'Налаштування API key для viber бота';
+$txt [ 'viber_api'] = 'Поточний ключ <div class = "smalltext"> Без пробілів. </ Div>';
+$txt [ 'viber_api_set'] = '451e54f24e1002ee-170c2356302871eb-68a6fe79d548fb9c';
+$txt [ 'viber_api_desc'] = 'Необхідно додати API key viber:'. $txt [ 'viber_api_set'];
